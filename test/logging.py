c91f0460fefc2ae5422a59e286002d18a243f809
jc <chenjd98@163.com>
2019-11-01 16:41:34 -0500

    add logging system

--- test/logging.py
+++ test/logging.py
@@ -1,13 +1,32 @@
 import logging
+import logging.handlers
+import os
+import utils.config as config
 
 
-logging.basicConfig(
-    format="%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s",
-    datefmt="%d-%m-%Y %H:%M:%S",
-    level=logging.DEBUG,  # lowest level to show in console
-    filename="logs/logs.txt"
-)
+os.makedirs(config.LOG_DIR, exist_ok=True)
+formatter = logging.Formatter(**config.LOG_FORMAT)
+file_handler = logging.handlers.RotatingFileHandler(config.LOG_FILE,
+                                                    maxBytes=config.LOG_SIZE,
+                                                    backupCount=config.LOG_CNT)
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+logging.getLogger().addHandler(file_handler)
+logger = logging.getLogger(__name__)
+logger.info('log test 2')
+"""
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+    logging.basicConfig(level=logging.INFO, filename=LOG_FILE, format=LOG_FORMAT)
+    logging.info("\n\n-------------------- start -----------------------\n")
+    logging.basicConfig(
+        format=LOG_FORMAT,
+        datefmt="%d-%m-%Y %H:%M:%S",
+        level=logging.DEBUG,  # lowest level to show in console
+        filename="logs/logs.txt"
+    )
 
 logger = logging.getLogger(__name__)
 
 logger.info("new info")
+"""
