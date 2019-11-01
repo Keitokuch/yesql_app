536678cc58f5a9f50681544d3baf813df50bae4c
JC <jingdec2@illinois.edu>
2019-10-31 23:54:41 -0500

    Add db insert and fix update

--- database/mysql.py
+++ database/mysql.py
@@ -11,7 +11,7 @@ def get_jname_by_id(jid: int):
                     ;
                     """
             cursor.execute(query, {'jid': jid})
-            return cursor.fetchall()
+            return cursor.fetchone()
 
 
 def get_jid_name():
@@ -24,6 +24,20 @@ def get_jid_name():
                     ;
             """
             cursor.execute(query)
+            return cursor.fetchall()
+
+
+def search_journal_by_name(keyword: str):
+    with MySQLConnection() as conn:
+        with conn.cursor() as cursor:
+            query = """
+                    SELECT *
+                    FROM jid_name
+                    WHERE journal_name LIKE %s
+                    LIMIT 15
+                    ;
+                    """
+            cursor.execute(query, "%" + keyword + "%")
             return cursor.fetchall()
 
 
@@ -50,4 +64,14 @@ def delete_journal_by_id(jid: int):
                     ;
             """
             cursor.execute(query, {'jid': jid})
-            return 0
+
+
+def insert_journal_name(jname: str):
+    with MySQLConnection() as conn:
+        with conn.cursor() as cursor:
+            query = """
+                    INSERT INTO jid_name (journal_name)
+                    VALUE (%(jname)s)
+                    ;
+                    """
+            cursor.execute(query, {'jname': jname})
