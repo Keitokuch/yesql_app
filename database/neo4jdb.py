12e6fa89be551ff90008c62fb2a62a86388a7198
Chenhao Zhang <chenhao3@illinois.edu>
2019-12-05 05:58:33 -0600

    fix syntax and add most relevant articles return

--- database/neo4jdb.py
+++ database/neo4jdb.py
@@ -6,10 +6,10 @@ driver = GraphDatabase.driver(uri="bolt://localhost:7687", auth=("neo4j", "8888"
 
 def add_read_articles(uid, aid):
     with driver.session() as session:
-        session.run("MERGE (u:User {id: $uid})", uid=uid)
-        session.run("MERGE (a:Article {id: $read_article_id})", read_article_id=aid)
+        session.run("MERGE (u:User {id: {uid}})", uid=uid)
+        session.run("MERGE (a:Article {id: {read_article_id}})", read_article_id=aid)
         result = session.run(
-            "MATCH (u:User {id: $uid}), (a:Article {id: $read_article_id}) MERGE (u)-[:Read]->(a)",
+            "MATCH (u:User {id: {uid}}), (a:Article {id: {read_article_id}}) MERGE (u)-[:Read]->(a)",
             uid=uid, read_article_id=aid
         )
         result = list(result)
@@ -18,9 +18,9 @@ def add_read_articles(uid, aid):
 
 def add_like_articles(uid, aid):
     with driver.session() as session:
-        session.run("MERGE (a:Article {id: $like_article_id})", like_article_id=aid)
+        session.run("MERGE (a:Article {id: {like_article_id}})", like_article_id=aid)
         result = session.run(
-            "MATCH (u:User {id: $uid}), (a:Article {id: $like_article_id}) MERGE (u)-[:Like]->(a)",
+            "MATCH (u:User {id: {uid}}), (a:Article {id: {like_article_id}}) MERGE (u)-[:Like]->(a)",
             uid=uid, like_article_id=aid
         )
         result = list(result)
@@ -31,65 +31,70 @@ def find_similar_user_articles(uid):
     with driver.session() as session:
         # store read relation uid
         uid_res = session.run(
-            "MATCH (u1:User {id: $uid})-[r:Read]-(a:Article)-[:Like]-(u2:User) "
+            "MATCH (u1:User {id: {uid}})-[r:Read]-(a:Article)-[:Like]-(u2:User) "
             "RETURN u2.id, COUNT(r) "
             "ORDER BY COUNT(r) DESC",
             uid=uid
         )
 
-        uid_list = []
+        uid_dict = {}
         for record in uid_res:
-            uid_list.append(record["u2.id"])
+            uid_dict[record["u2.id"]] = record["COUNT(r)"]
 
         # store like relation uid
         uid_res = session.run(
-            "MATCH (u1:User {id: $uid})-[r:Like]-(a:Article)-[:Like]-(u2:User) "
+            "MATCH (u1:User {id: {uid}})-[r:Like]-(a:Article)-[:Like]-(u2:User) "
             "RETURN u2.id, COUNT(r) "
             "ORDER BY COUNT(r) DESC",
             uid=uid
         )
         for record in uid_res:
-            uid_list.append(record["u2.id"])
+            uid_dict[record["u2.id"]] = record["COUNT(r)"]
 
-        print(uid_list, "sdsd")
-
+        # arrange uid_list by most similar user first
+        uid_list = find_most_similar_users(uid_dict)
 
         article_res = session.run(
             "MATCH (u:User)-[:Like]-(a:Article) "
-            "WHERE u.id IN $uid_list "
-            "RETURN a.id "
-            "LIMIT 8",
+            "WHERE u.id IN {uid_list} "
+            "RETURN a.id, u.id ",
             parameters={'uid_list': uid_list}
         )
-        res = []
+
+        # find most relevant articles and only return those under the limit value
+        article_list = []
         for record in article_res:
-            res.append(record["a.id"])
-        print(res)
+            temp = (record["a.id"], record["u.id"])
+            article_list.append(temp)
+        res = find_most_relevant_articles(article_list, uid_list, 8)
         return res
 
 
 def user_liked_article(uid, aid):
     with driver.session() as session:
         res = session.run(
-            "MATCH (u:User {id: $uid}), (a:Article {id: $aid}) "
+            "MATCH (u:User {id: {uid}}), (a:Article {id: {aid}}) "
             "RETURN EXISTS((u)-[:Like]-(a)) ",
             uid=uid, aid=aid
         )
         return res.value()[0]
 
 
+def find_most_similar_users(uid_dict):
+    sorted_list = sorted(uid_dict.items(), key=lambda kv: -kv[1])
+    res = []
+    for item in sorted_list:
+        res.append(item[0])
+    return res
 
-# clear_all_records()
-# add_read_articles(1, 3)
-# add_read_articles(3, 3)
-# add_like_articles(3, 3)
-# add_like_articles(1, 4)
-# add_read_articles(1, 5)
-# add_read_articles(1, 6)
-# add_read_articles(2, 4)
-# add_like_articles(2, 3)
-#
-# add_read_articles(2, 8)
-# add_like_articles(1, 8)
-# find_similar_user_articles(2)
+
+def find_most_relevant_articles(article_dict, uid_list, limit):
+    res = []
+    for idx, uid in enumerate(uid_list):
+        for temp_tuple in article_dict:
+            if temp_tuple[1] == uid and len(res) < limit:
+                res.append(temp_tuple[0])
+    return res
+
+
 
